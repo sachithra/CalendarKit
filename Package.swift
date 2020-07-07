93910850a82718519828f9ce9dee7fe48c299556
Sachithra Udayanga <sachithragt@gmail.com>
2020-07-07 17:58:52 +0530

    Package set to objc

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@
      dependencies: [
          // Dependencies declare other packages that this package depends on.
          // .package(url: /* package url */, from: "1.0.0"),
-         .package(url: "https://github.com/maniramezan/DateTools.git", .branch("master"))
+         .package(url: "https://github.com/maniramezan/DateTools.git", .branch("objc"))
      ],
      targets: [
          // Targets are the basic building blocks of a package. A target can define a module or a test suite.
